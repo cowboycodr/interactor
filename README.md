294115727d8035f2b68d54cd24244f3c11daea7e
Kian McKenna <59800761+cowboycodr@users.noreply.github.com>
2022-01-26 23:35:26 -0600

    update: README // install instructions

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # interactor
+
+## Installation
+### MacOS
+```
+python3 -m pip install api-interactor
+```
+
+### Windows
+```
+py -3 -m pip install api-interactor
+```
+
 #### This project was constructed with [Choam](https://github.com/cowboycodr/choam)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="api-interactor",
+    version="0.0.1",
+    description="",
+    packages=find_packages(),
+    keywords=[],
+    install_requires=['choam', 'wheel', 'requests', 'twine'],
+    project_urls={
+        'Source': 'https://github.com/cowboycodr/interactor'
+    },
+)
